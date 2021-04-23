07aab282416c3bcaf1ce5f62f143a7bf08194ef7
Kevin Rodriguez <seidaikevin@gmail.com>
2021-04-23 13:47:29 -0500

    Add: Logic to get next state

--- getDFAStates.py
+++ getDFAStates.py
@@ -1,3 +1,20 @@
 
-def get_state(set, nfa_states, key):
-    pass
+def get_state(a_set, nfa_states, key):
+    result = set()
+    for value in a_set:
+        if cur_set := nfa_states[value].get(key):
+            result.update(cur_set)
+    while extra := get_transitions(result, nfa_states):
+        result.update(extra)
+    return result
+
+
+def get_transitions(a_set, nfa_states):
+    extra = set()
+    for value in a_set:
+        if cur_set := nfa_states[value].get("void"):
+            if cur_set not in a_set:
+                extra.update(cur_set)
+    if extra.issubset(a_set):
+        return
+    return extra
