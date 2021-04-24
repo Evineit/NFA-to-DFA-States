e48b877ad6bcce7c7585b65470b892f1c33702e7
Kevin Rodriguez <seidaikevin@gmail.com>
2021-04-24 18:54:18 -0500

    Better print in get_all_states, add logic to solve a NFA to DFA

--- getDFAStates.py
+++ getDFAStates.py
@@ -1,14 +1,46 @@
+from pprint import pprint
 
-def get_all_states(initial_state, nfa_states, keys):
+def solve(locket_e, nfa_states, keys):
+    solution = {
+        'A': locket_e
+    }
     states = []
-    to_print = "\n"
+    states.append(locket_e)
+    loop_num = 0
+    for state in states:
+        letter = chr(ord('A')+loop_num)
+        loop_num += 1
+        temp_list = get_all_states(state, nfa_states, keys, letter, states)
+        for state_set in temp_list:
+            if state_set not in states:
+                states.append(state_set)
+                # print(f"estado {chr(ord('A')+len(states))} - {state_set} fue agregado")
+    for state in states:
+        if state not in solution.values():
+            solution[chr(ord(list(solution.keys())[-1])+1)] = state
+    pprint(solution)
+    return solution
+
+
+def get_all_states(initial_state, nfa_states, keys, context_state_key, context_states = []) -> list:
+    states = []
+    new_states = []
+    to_print = ""
     for key in keys:
         new_state = get_state(initial_state, nfa_states, key)
+        if new_state in context_states:
+            to_print += f"{context_state_key}, {key} = {new_state} = {chr(ord('A')+context_states.index(new_state))}\n" if new_state else f"{context_state_key}, {key} = {{}}\n"
+        else:
+            to_print += f"{context_state_key}, {key} = {new_state} = {chr(ord('A')+len(context_states)+len(new_states))} * \n" if new_state else f"{context_state_key}, {key} = {{}}\n"
         if new_state and new_state not in states:
             states.append(new_state)
-            to_print+= f"{key} = {new_state}\n"
+            if new_state not in context_states:
+                new_states.append(new_state)
     print(to_print)
+    print("\n")
+    pprint(states)
     return states
+
 
 def get_state(a_set, nfa_states, key):
     result = set()
@@ -17,7 +49,6 @@ def get_state(a_set, nfa_states, key):
             result.update(cur_set)
     while extra := get_transitions(result, nfa_states):
         result.update(extra)
-    print(f"\n{key} = {result}")
     return result
 
 
@@ -30,3 +61,6 @@ def get_transitions(a_set, nfa_states):
     if extra.issubset(a_set):
         return
     return extra
+
+if __name__ == "__main__":
+    pass
